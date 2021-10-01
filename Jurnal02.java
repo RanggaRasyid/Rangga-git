fcde5162ba93cc5a6a3459d0c081d1872e9ac1bc
abdul rasyid ranggdaiva <ranggarasyid29@gmail.com>
2021-10-01 14:36:09 +0700

    Jurnal02

---
+++ Jurnal02.java
@@ -0,0 +1,7 @@
+public class Jurnal02 {
+	public static void main (String[] args) {
+		System.out.println("NIM   : 6706210128");
+		System.out.println("Nama  : Abdul Rasyid Ranggadiva");
+		System.out.println("Kelas : D3IF-45-04");
+	}
+}
