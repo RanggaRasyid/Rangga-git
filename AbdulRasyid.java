c3567e212088aedc736ef666a142622659ab0554
abdul rasyid ranggdaiva <ranggarasyid29@gmail.com>
2021-10-01 16:10:14 +0700

    Solusi Jurnal022'

---
+++ AbdulRasyid.java
@@ -0,0 +1,8 @@
+public class AbdulRasyid {
+
+	public static void main (String[] args) {
+		System.out.println ("Hai perkenalkan nama saya ABDUL RASYID RANGGADIVA");
+		System.out.println ("Asal saya dari SERANG BANTEN");
+		System.out.println ("Umur saya 18 tahun, hobby saya bermain game");
+	}
+}
